b07f9ae35442acaf6ed25654668ab5a21dd0b7dc
Ronaldding <ronaldding2002@gmail.com>
2024-01-30 17:56:55 +0800

    finishewd lab 4

--- Lab_4/follower.py
+++ Lab_4/follower.py
@@ -1,10 +1,15 @@
+# follower.py
 #!/usr/bin/env python  
 # -*- coding: utf-8 -*-
 
 import sys
 import rospy
 # TODO 1: import math to use atan2() easily.
+import math
 # TODO 2: import all message types you need.
+from geometry_msgs.msg import Twist
+from eleg_t03topic_yoursid.msg import Mypose
+from turtlesim.msg import Pose
 
 def pose_callback(pose):
     global ROBOT_X
@@ -13,6 +18,9 @@ def pose_callback(pose):
 
     rospy.loginfo("Robot 2 X = %f: Y=%f: Z = %f\n", pose.x, pose.y, pose.theta)
     # TODO 3: assign the robot pose to the global variables.
+    ROBOT_X = pose.x
+    ROBOT_Y = pose.y
+    ROBOT_T = pose.theta
 
 
 def myPose_callback(myPose):
@@ -21,18 +29,46 @@ def myPose_callback(myPose):
     #          2. Make the angle of the robot2 always face the other robot. 
     #          3. Think about how to calculate the difference of two angles using atan2()
 
+    # P-Control
+    x_diff = myPose.x - ROBOT_X
+    y_diff = myPose.y - ROBOT_Y
+    t_diff = math.atan2(y_diff, x_diff) - ROBOT_T
+
+    if t_diff > math.pi:
+        t_diff = t_diff - 2 * math.pi
+    elif t_diff < -math.pi:
+        t_diff = t_diff + 2 * math.pi
+
+    linear_diff = math.sqrt(math.pow(x_diff, 2)+ math.pow(y_diff, 2))
+
+    k = 0.2
+
+    vel.linear.x = 0.2 * abs(x_diff)
+    vel.linear.y = 0
+    vel.linear.z = 0
+    vel.angular.x = 0
+    vel.angular.y = 0
+    vel.angular.z = 5 * t_diff
+
 
 if __name__ == '__main__':
     # TODO 5: initialize node 'follow_turtle'
+    rospy.init_node('follow_turtle', anonymous=False)
     # TODO 6: define a publisher: topic name is '/turtle2/cmd_vel'. 
+    pub = rospy.Publisher( '/turtle2/cmd_vel', Twist, queue_size=10) 
     # TODO 7: define a subscriber: topic name is '/turtle1/Mypose'. 
+    rospy.Subscriber( '/turtle1/Mypose', Mypose, myPose_callback)  
     # TODO 8: define a subscriber: topic name is '/turtle2/pose'. 
+    rospy.Subscriber( '/turtle2/pose', Pose, pose_callback)  
 
     global vel
     # TODO 9: initialize the variable vel with the message type.  
+    vel = Twist()
+
     rate = rospy.Rate(10)       
     while not rospy.is_shutdown():
         # TODO : publish vel.
+        pub.publish(vel)
 
         rate.sleep()
 
--- Lab_4/publisher.py
+++ Lab_4/publisher.py
@@ -1,3 +1,4 @@
+# publisher.py
 #!/usr/bin/env python  
 # -*- coding: utf-8 -*-
 
@@ -7,25 +8,36 @@ import sys
 import rospy
 
 # TODO 1: import turtlesim/Pose message.
+from turtlesim.msg import Pose
 # TODO 2: import your Mypose message.
+from eleg_t03topic_yoursid.msg import Mypose
 
 
 def pose_callback(pose):
     rospy.loginfo("Robot 1 X = %f: Y=%f: Z = %f\n", pose.x, pose.y, pose.theta)
     # TODO 3: assign the subscribed pose to your mp.x, mp.y, mp.theta.
+    mp.x = pose.x
+    mp.y = pose.y
+    mp.theta = pose.theta
+
 
 # Please check with the lecture slides to see how to init a node. You need to do it without TA's help in order to get a full marks for this task.
 if __name__ == '__main__':
     # TODO 4: initialize node 'pub_turtle'. 
+    rospy.init_node('pub_turtle', anonymous=False)
     # TODO 5: define a publisher: topic name is '/turtle1/Mypose'. 
+    pub = rospy.Publisher( '/turtle1/Mypose', Mypose, queue_size=10)  
+
     # TODO 6: define a subscriber: topic name is '/turtle1/pose'. 
+    rospy.Subscriber( '/turtle1/pose', Pose, pose_callback) 
 
     global mp
     # TODO 7: initialize the variable mp with your message type.  
+    mp = Mypose()
 
     rate = rospy.Rate(10) 
     while not rospy.is_shutdown():
         # TODO : publish mp.
-
-        rate.sleep()             
+        pub.publish(mp)
+        rate.sleep()
 
